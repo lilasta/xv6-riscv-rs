2f8ba2637cf09f5c7a7708eedb3ba22c3e11debd
woppopo <woppopo@protonmail.com>
2022-06-22 08:17:02 +0900

    Doc: entry

--- kernel/src/entry.rs
+++ kernel/src/entry.rs
@@ -1,28 +1,36 @@
+//! 最初に実行される処理
+//!
+//! `qemu -kernel`によってカーネルは0x80000000番地にロードされ、各CPUはそこにジャンプする。
+//! linker.ldによって、この処理のコードは0x80000000番地に配置される。
+//!
+//! `sp = stack0 + (hartid * 4096)`
+//!
+//! この処理は、各CPUにそれぞれ4096バイトのスタック領域を割り当て、start関数を呼び出す。
+
 use core::arch::global_asm;
 
 global_asm!(
     r#"
-	 .attribute arch, "rv64gc"
-	 # qemu -kernel loads the kernel at 0x80000000
-	 # and causes each CPU to jump there.
-	 # kernel.ld causes the following code to
-	 # be placed at 0x80000000.
+# FIXME: LLVMがRISC-Vの拡張機能を認識していない？
+# https://github.com/rust-lang/rust/issues/80608
+.attribute arch, "rv64gc"
+
 .section .text
 .global _entry
 _entry:
-# set up a stack for C.
-	 # stack0 is declared in start.c,
-	 # with a 4096-byte stack per CPU.
-	 # sp = stack0 + (hartid * 4096)
-	 la sp, stack0
-	 li a0, 1024*4
-csrr a1, mhartid
-	 addi a1, a1, 1
-	 mul a0, a0, a1
-	 add sp, sp, a0
-# jump to start() in start.c
-	 call start
+    # a0 = 4096 * (mhartid + 1)
+    csrr a1, mhartid
+    addi a1, a1, 1
+    li a0, 1024*4
+    mul a0, a0, a1
+
+    # sp = stack0 + a0
+    la sp, stack0
+    add sp, sp, a0
+
+    # start()
+    call start
 spin:
-	 j spin
+    j spin
 "#
 );
