6cb729507fe46767c74f7c79d3e5ac78799ab8ca
woppopo <woppopo@protonmail.com>
2022-06-23 10:51:21 +0900

    config

---
+++ kernel/src/config.rs
@@ -0,0 +1,38 @@
+// maximum number of processes
+pub const NPROC: usize = 64;
+
+// maximum number of CPUs
+pub const NCPU: usize = 8;
+
+// open files per process
+pub const NOFILE: usize = 16;
+
+// open files per system
+pub const NFILE: usize = 100;
+
+// maximum number of active i-nodes
+pub const NINODE: usize = 50;
+
+// maximum major device number
+pub const NDEV: usize = 10;
+
+// device number of file system root disk
+pub const ROOTDEV: usize = 1;
+
+// max exec arguments
+pub const MAXARG: usize = 32;
+
+// max # of blocks any FS op writes
+pub const MAXOPBLOCKS: usize = 10;
+
+// max data blocks in on-disk log
+pub const LOGSIZE: usize = MAXOPBLOCKS * 3;
+
+// size of disk block cache
+pub const NBUF: usize = MAXOPBLOCKS * 3;
+
+// size of file system in blocks
+pub const FSSIZE: usize = 1000;
+
+// maximum file path name
+pub const MAXPATH: usize = 128;
